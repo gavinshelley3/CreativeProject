968f20402a22ccad6834c62bfcdd1b581a89c542
Ethan Kerr <elkerr97@gmail.com>
2022-10-28 00:21:24 +0000

    Add react functionality to home.html

--- js/homeScript.js
+++ js/homeScript.js
@@ -4,4 +4,24 @@ class Home extends React.Component {
         super(props);
         
     }
+    
+    render() {
+        return (
+            <div id="home-content">
+        <div>
+          <h1>Get the Sneakiest Deals</h1>
+        </div>
+
+        <p>
+          Subscribe to be the first to know about the biggest, sneakiest deals
+        </p>
+
+        <div id = "subscribe"><button class="button" >Subscribe</button></div>
+      </div>
+            
+            )
+    }
+    
+    const root = ReactDOM.createRoot(document.getElementById("home-inside"));
+    root.render(<Home />);
 }
