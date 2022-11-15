f5eec3412abf1efc30b6e0de7d7279c88bae9ef2
Ubuntu <ubuntu@ip-172-31-47-162.us-west-2.compute.internal>
2022-11-15 22:08:21 +0000

    Start implementing App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,29 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+const SneaksAPI = require("sneaks-api");
 
 function App() {
+  
+  // setup state
+  const [shoes, setShoes] = useState([]);
+  const [error, setError] = useState("");
+  const [cartItem, setCartItems] = useState([]);
+
+  const fetchCart = async() => {
+    try{
+      const response = await axios.get("/api/cart");
+      setCartItems(response.data);
+    } catch(error) {
+      setError("Error retrieving cart items: " + error);
+    }
+  };
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
